f461c92f2a56c9301669a3a41b8eb80b787b957e
Ignasi <ignasirv@hotmail.com>
2018-10-24 13:16:40 +0200

    New cal to register a new hardwareID

--- contracts/HardwareData.sol
+++ contracts/HardwareData.sol
@@ -2,10 +2,23 @@ pragma solidity ^0.4.24;
 
 /** @title HardwareData */
 
+import "./User.sol";
+
 contract HardwareData {
 
+    User private user;
     bytes[] public ipfsHashes;
     event newHash(bytes ipfsHash);
+    mapping(address => bytes) public addressToHardwareId;
+
+   /**
+    * @dev Throws if non-user is trying to interact with the contract method.
+    */
+    modifier onlyUser() {
+        require(user.hasUser(msg.sender), "You need to have a user for calling this function");
+        _;
+    }
+    
 
     /**
     * @dev Add a new hash from ipfs to ethereum
@@ -21,4 +34,11 @@ contract HardwareData {
         return ipfsHashes.length;
     }
 
+    function addNewHardwareId(bytes hardware_id) public {
+        addressToHardwareId[msg.sender] = hardware_id;
+    }
+
+    function getHardwareIdFromSender() public view returns (bytes hardwareId){
+        return addressToHardwareId[msg.sender];
+    }
 }
--- test/HardwareTests.js
+++ test/HardwareTests.js
@@ -1,4 +1,7 @@
 const HardwareData = artifacts.require("shasta-os/HardwareData");
+const User = artifacts.require("shasta-os/User");
+const ShastaMarket = artifacts.require("shasta-os/ShastaMarket")
+
 const Promise = require("bluebird");
 
 
@@ -7,15 +10,42 @@ contract('HardwareTests', function (accounts) {
     const owner = accounts[0];
     const user = accounts[1]
     let HardwareInstance;
+    let UserInstance;
     let randomHash = web3.utils.utf8ToHex("aRandomHash");
+    const randomHardwareId = web3.utils.utf8ToHex("randomHardwareId");
+    const randomUsername = web3.utils.utf8ToHex("randomUsername");
 
-    before('Initialize contract with Web3', async function () {
+    before('Initialize contracts with Web3', async function () {
+
+        const MarketContract = await new web3.eth.Contract(ShastaMarket.abi);
+        const marketGas = await MarketContract.deploy({ data: ShastaMarket.bytecode }).estimateGas({ from: owner });
+        MarketInstance = await MarketContract.deploy({ data: ShastaMarket.bytecode }).send({ from: owner, gas: marketGas });
 
         const HardwareContract = await new web3.eth.Contract(HardwareData.abi);
         const hardwareGas = await HardwareContract.deploy({ data: HardwareData.bytecode }).estimateGas({ from: owner });
         HardwareInstance = await HardwareContract.deploy({ data: HardwareData.bytecode }).send({ from: owner, gas: hardwareGas });
         
+        const UserContract = await new web3.eth.Contract(User.abi);
+        const userGas = await UserContract.deploy({ data: User.bytecode, arguments: [MarketInstance.options.address] }).estimateGas({ from: owner });
+        UserInstance = await UserContract.deploy({ data: User.bytecode, arguments: [MarketInstance.options.address] }).send({ from: owner, gas: userGas });
+       
     });
+
+    it('Should create a user and add a new hardware to that user', async function () {
+
+        //Create a new user
+        const gas = await UserInstance.methods.createUser(randomUsername, randomHash).estimateGas({ from: user });
+        await UserInstance.methods.createUser(randomUsername, randomHash).send({ from: user, gas: gas });
+
+        //Add new hardware to the created user
+        const hardwareGas = await HardwareInstance.methods.addNewHardwareId(randomHardwareId).estimateGas({ from: user });
+        await HardwareInstance.methods.addNewHardwareId(randomHardwareId).send({ from: user, gas: hardwareGas });
+        
+        //Check the hardware exists for the created user
+        const hardwareId = await HardwareInstance.methods.getHardwareIdFromSender().call({ from: user });
+        assert.equal(randomHardwareId, hardwareId);
+    })
+
 
     it('Should add a hash to the smart contract and check it exists', async function () {
 
